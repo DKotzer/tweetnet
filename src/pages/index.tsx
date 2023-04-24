8ac80648c761c09d487147bb88591790504a36b8
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-04-24 03:25:38 -0400

    render posts test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -28,6 +29,11 @@ const Home: NextPage = () => {
             />
           )}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
