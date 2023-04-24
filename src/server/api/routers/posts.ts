5a355865dcf79e4e2972923a79f5037b453a782a
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-04-24 12:10:11 -0400

    backup

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,44 @@
+import type { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
+    });
+
+    const userId = posts.map((post) => post.authorId);
+    const users = (
+      await clerkClient.users.getUserList({
+        userId,
+        limit: 110,
+      })
+    ).map(filterUserForClient);
+    console.log(users);
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      ({
+        post,
+        author,
+      });
+    });
   }),
 });
+
+//  const author = users.find((user) => user.id === post.authorId);
+//  ({
+//    post,
+//    author,
+//  });
