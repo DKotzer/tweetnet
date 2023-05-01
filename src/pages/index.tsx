78304549a4309195c4af0179558e3be68892db45
Dylan <34380246+DKotzer@users.noreply.github.com>
2023-05-01 04:08:15 -0400

    basic pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,48 +5,74 @@ import { PageLayout } from "~/components/layout";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { BotPostView } from "~/components/botpostview";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
 
 const PostsFeed = () => {
-  const { data, isLoading } = api.bots.getAllPosts.useQuery();
+  const [currentPage, setCurrentPage] = useState(0);
+  const postsPerPage = 50;
+
+  const { data, isLoading } = api.bots.getAllPosts.useQuery({
+    page: currentPage + 1,
+    per_page: postsPerPage,
+  });
+
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    setCurrentPage(selected);
+  };
 
   // console.log("bot data", data);
 
   if (isLoading)
     return (
-      <div className="w-screen border-x border-slate-400/50 md:w-[670px]">
-        <div className="flex h-[340px] items-center justify-center border-b border-slate-400/50">
-          <LoadingSpinner size={60} />
-        </div>
-        <div className="flex h-[360px] items-center justify-center border-b border-slate-400/50">
-          <LoadingSpinner size={60} />
-        </div>
-        <div className=" flex  h-[355px] items-center justify-center border-b border-slate-400/50">
-          <LoadingSpinner size={60} />
-        </div>
-        <div className="flex h-[340px] items-center justify-center border-b border-slate-400/50">
-          <LoadingSpinner size={60} />
-        </div>
-        <div className="flex h-[360px] items-center justify-center border-b border-slate-400/50">
-          <LoadingSpinner size={60} />
-        </div>
-        <div className=" flex  h-[355px] items-center justify-center border-b border-slate-400/50">
-          <LoadingSpinner size={60} />
-        </div>
-
-        {/* <div className="absolute top-0 right-0 flex h-screen w-screen items-center justify-center">
-          
-        </div> */}
-      </div>
+      <div className="w-screen border-x border-slate-400/50 md:w-[670px]"></div>
     );
   if (!data) return <div>Something went wrong, no data found</div>;
 
-  if (data.length < 1) return <div>No one has posted yet</div>;
+  if (!isLoading && data.posts.length < 1)
+    return <div>No one has posted yet</div>;
+
+  if (!isLoading && data.posts.length > 0)
+    return (
+      <>
+        <div className="flex flex-col">
+          {data.posts.map((fullPost) => (
+            <BotPostView
+              {...fullPost}
+              key={fullPost.id}
+              username={fullPost.authorName}
+              image={fullPost.authorImage}
+              postImage={fullPost.postImage || ""}
+              originalPostId={fullPost.originalPostId || ""}
+            />
+          ))}
+        </div>
+        <ReactPaginate
+          pageCount={Math.ceil(data.total / postsPerPage)}
+          marginPagesDisplayed={3}
+          pageRangeDisplayed={2}
+          onPageChange={handlePageChange}
+          containerClassName={"flex justify-center mt-8"}
+          pageClassName={"mr-2"}
+          activeClassName={"text-white bg-blue-500 rounded-lg"}
+          pageLinkClassName={"p-2 rounded-lg hover:bg-blue-200"}
+          previousClassName={"mr-2"}
+          nextClassName={"ml-2"}
+          previousLabel={"Prev"}
+          nextLabel={"Next"}
+          disabledClassName={"text-gray-500 pointer-events-none"}
+          forcePage={currentPage}
+        />
+      </>
+    );
+
+  if (isLoading) return <LoadingSpinner />;
 
   // console.log('data test', data)
 
   return (
     <div className="flex flex-col">
-      {data.map((fullPost) => (
+      {data.posts.map((fullPost) => (
         <BotPostView
           {...fullPost}
           key={fullPost.id}
@@ -55,8 +81,22 @@ const PostsFeed = () => {
           postImage={fullPost.postImage || ""}
           originalPostId={fullPost.originalPostId || ""}
         />
-        // <PostView {...fullPost} key={fullPost.post.id} />
       ))}
+      <ReactPaginate
+        pageCount={data.total / postsPerPage}
+        marginPagesDisplayed={3}
+        pageRangeDisplayed={2}
+        onPageChange={handlePageChange}
+        containerClassName={"flex justify-center mt-8"}
+        pageClassName={"mr-2"}
+        activeClassName={"text-white bg-blue-500 rounded-lg"}
+        pageLinkClassName={"p-2 rounded-lg hover:bg-blue-200"}
+        previousClassName={"mr-2"}
+        nextClassName={"ml-2"}
+        previousLabel={"Prev"}
+        nextLabel={"Next"}
+        disabledClassName={"text-gray-500 pointer-events-none"}
+      />
     </div>
   );
 };
