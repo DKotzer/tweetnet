370981a459f36522fd37a2b27b5ee0f96887ce00
Dylan <34380246+DKotzer@users.noreply.github.com>
2024-11-03 14:13:55 -0500

    cascade post styling fixes

--- src/components/botpostviewcascade.tsx
+++ src/components/botpostviewcascade.tsx
@@ -7,236 +7,22 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingSpinner } from "./loading";
 import React, { Fragment } from "react";
 import LinkPreview from "./linkPreview";
+import { CustomLi, CustomText, CustomList } from "./CustomComponents";
+
 
 dayjs.extend(relativeTime);
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/";
-
-interface CustomLiProps {
-  children: React.ReactNode;
-  type: "li";
-}
-
-const CustomLi: React.FC<CustomLiProps> = ({ children }) => {
-  const content = (children as string[])[0];
-  let output = [];
-
-  let hashtags = [] as string[];
-
-  if (content) {
-    const listItems = content.split(/\n/).filter((item) => item.trim() !== "");
-
-    listItems.forEach((item, itemIndex) => {
-      const segments = item.split(/(\s+)/);
-      const itemOutput = segments.map((segment, index) => {
-        if (segment.startsWith("#")) {
-          const hashtagMatch = segment.slice(1).match(/[a-zA-Z0-9_]*/);
-          const hashtag = hashtagMatch ? hashtagMatch[0] : "";
-          if (hashtag === "") {
-            return <Fragment key={`segment-${index}`}>{segment}</Fragment>;
-          } else {
-            hashtags.push(hashtag); // Collect hashtags for later use
-            return null;
-          }
-        } else {
-          return <Fragment key={`segment-${index}`}>{segment}</Fragment>;
-        }
-      });
-
-      if (itemOutput.length > 0) {
-        output.push(<li key={`item-${itemIndex}`}>{itemOutput}</li>);
-      }
-    });
-
-    // Generate the hashtags section
-    const hashtagsOutput = hashtags.map((hashtag, index) => (
-      <Fragment key={`hashtag-${index}`}>
-        <Link
-          className="hashTag"
-          href={`${baseURL}hashtag/${hashtag.substring(1)}`}
-        >
-          #{hashtag}
-        </Link>{" "}
-      </Fragment>
-    ));
-
-    if (hashtags.length > 0) {
-      output.push(
-        <p key="hashtags" className="hashtags">
-          {hashtagsOutput}
-        </p>
-      );
-    }
-  } else {
-    output =
-      content?.trim() !== "" ? [<li key="single-item">{content}</li>] : [];
-  }
-
-  return <span>{output}</span>;
-};
 
 interface CustomTextProps {
   children: React.ReactNode;
-  type: "p" | "span" | "li" | "a" | "href";
+  type: "p" | "span" | "li" | "a" | "href" | "link" | "text";
 }
-
-const CustomText: React.FC<CustomTextProps> = ({ children }) => {
-  let content = (children as string[]).join("");
-  // Filter out [object Object]
-  content = content.replace(/\[object Object\]/g, "");
-
-  let hashtags: any = [];
-  const output =
-    content && content.length !== 0 ? (
-      <>
-        {content.split("\n").map((line, i) => {
-          const segments = line.split(/(\s+)/);
-          const paragraphOutput = segments.map((segment, j) => {
-            if (segment.startsWith("#")) {
-              const hashtag = segment.match(/#[a-zA-Z0-9_]*/)?.[0] || "";
-              if (hashtag) {
-                return (
-                  <Fragment key={`segment-${j}`}>
-                    <Link
-                      className="hashTag"
-                      href={`${baseURL}hashtag/${hashtag.substring(1)}`}
-                    >
-                      {hashtag}
-                    </Link>{" "}
-                  </Fragment>
-                );
-              }
-            } else if (segment.startsWith("@")) {
-              const username =
-                segment.match(/@[a-zA-Z0-9_]*/)?.[0]?.slice(1) || "";
-              if (username) {
-                return (
-                  <Fragment key={`segment-${j}`}>
-                    <Link
-                      className="tweetName"
-                      href={`${baseURL}bot/${username}`}
-                    >
-                      {segment}
-                    </Link>{" "}
-                  </Fragment>
-                );
-              }
-            } else if (
-              segment.startsWith("http") ||
-              segment.startsWith("www")
-            ) {
-              let url = segment;
-              if (
-                url.startsWith("'") ||
-                url.startsWith('"') ||
-                url.startsWith("(")
-              ) {
-                url = url.slice(1);
-              }
-              if (url.endsWith("'") || url.endsWith('"') || url.endsWith(")")) {
-                url = url.slice(0, -1);
-              }
-              return (
-                <Fragment key={`segment-${j}`}>
-                  <div className="max-w-[260px] overflow-hidden md:max-w-full">
-                    <LinkPreview url={url} />
-                  </div>
-                </Fragment>
-              );
-            } else if (segment.length < 1) {
-              return null;
-            } else {
-              return <Fragment key={`segment-${j}`}>{segment}</Fragment>;
-            }
-          });
-
-          return <p key={`paragraph-${i}`}>{paragraphOutput}</p>;
-        })}
-
-        {hashtags.length > 0 && (
-          <div className="hashtag-container">
-            {hashtags.map((hashtag: any, index: number) => (
-              <Fragment key={`hashtag-${index}`}>
-                {index > 0 && " "} {/* Add space between hashtags */}
-                <Link
-                  className="hashTag"
-                  href={`${baseURL}hashtag/${hashtag.substring(1)}`}
-                >
-                  {hashtag}
-                </Link>{" "}
-                {/* Include "#" symbol and make it a link */}
-              </Fragment>
-            ))}
-          </div>
-        )}
-      </>
-    ) : null;
-
-  return <div className="markdown text-lg">{output}</div>;
-};
 
 interface CustomListProps {
   children: React.ReactNode;
   type: "ul" | "ol";
 }
 
-const CustomList: React.FC<CustomListProps> = ({ children, type }) => {
-  const content = (children as string[])[0];
-  let output;
-  const hashtags: string[] = [];
-
-  if (content) {
-    const items = content
-      .replace(`<${type}>`, "")
-      .replace(`</${type}>`, "")
-      .split("\n")
-      .filter((item) => item.trim().length > 0);
-
-    // console.log("items:", items);
-
-    const listItems = items.map((item, index) => {
-      // console.log("item:", item);
-      // console.log("hashtags:", hashtags);
-      // Extract hashtags from each list item
-      const listItemText = item
-        .replace(/#\w+/g, (match) => {
-          hashtags.push(match.slice(1));
-          return "";
-        })
-        .trim();
-
-      // console.log("listItemText:", listItemText);
-
-      return <li key={`list-item-${index}`}>{listItemText}</li>;
-    });
-
-    output = (
-      <div className="markdown">
-        {type === "ul" ? <ul>{listItems}</ul> : <ol>{listItems}</ol>}
-        {hashtags.length > 0 && (
-          <p className="hashtag-container">
-            {hashtags.map((tag, index) => (
-              <Link
-                className="hashTag"
-                href={`http://localhost:3000/hashtag/${tag}`}
-                key={`hashtag-${index}`}
-              >
-                {`#${tag}`}
-              </Link>
-            ))}
-          </p>
-        )}
-      </div>
-    );
-  } else {
-    // console.log("else content:", content);
-    output = <div className="markdown">{content}</div>;
-  }
-
-  // console.log("output:", output);
-
-  return output;
-};
 
 type CustomComponents = {
   p: React.FC<CustomTextProps>;
@@ -507,7 +293,7 @@ export const BotPostViewCascade = (
                     />
                   </div>
 
-                  <div className="mb-3 flex gap-1 text-slate-400">
+                  <div className="mb-3 flex gap-1 flex-col ml-2 text-slate-400">
                     <Link
                       href={`/bot/@${data.authorName}`}
                       className="hoverUnderline hover:scale-105"
@@ -516,7 +302,7 @@ export const BotPostViewCascade = (
                     </Link>
                     <span className="hoverUnderline my-auto font-thin hover:scale-105">
                       <Link href={`/post/${data.id}`}>
-                        {` · ${dayjs(data.createdAt).fromNow()}`}
+                        {` ${dayjs(data.createdAt).fromNow()}`}
                       </Link>
                     </span>
                   </div>
@@ -594,18 +380,17 @@ export const BotPostViewCascade = (
           <div>
             {repliesData.map((reply) => (
               <div key={reply.id} className="flex items-start">
-                <div className="mt-1 flex-shrink-0">
+                <div className="mt-1 flex-shrink-0 w-2">
                   {/* You can use any icon or symbol here */}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
+                    width="16"
+                    height="16"
                     className="text-white"
-                    fill="none"
+                    fill="currentColor"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
                   >
-               
                     <path d="m12.707 7.293-1.414 1.414L15.586 13H7V4H5v11h10.586l-4.293 4.293 1.414 1.414L19.414 14l-6.707-6.707z" />
                   </svg>
                 </div>
@@ -629,9 +414,9 @@ export const BotPostViewCascade = (
     <div className="botPostView hover:bg-[#ffffff08]">
       <div
         key={props.id}
-        className="flex gap-3 border-x border-b border-t border-slate-400/50 p-4 flex-col"
+        className="flex flex-col gap-3 border-x border-b border-t border-slate-400/50"
       >
-        <div className="flex flex-col">
+        <div className="flex flex-col p-4">
           <div className="flex flex-row gap-3">
             <div className="relative h-14 w-14 rounded-full ring-1 ring-slate-400/50 hover:scale-110 hover:ring">
               <Image
