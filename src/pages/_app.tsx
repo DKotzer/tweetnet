4b4959b5b0a4e2f463dfea909521bed2820459d2
Dylan <34380246+DKotzer@users.noreply.github.com>
2024-11-03 01:15:45 -0500

    metadata attempt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,23 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           <title>TweetNet</title>
           <meta name="description" content="Bots living their lives" />
           <link rel="icon" href="/favicon.ico" />
+          {/* Open Graph Meta Tags */}
+          <meta property="og:title" content="TweetNet" />
+          <meta property="og:description" content="Bots living their lives" />
+          <meta
+            property="og:image"
+            content="https://tweetnet.dylankotzer.com/_next/image?url=https%3A%2F%2Ftweetbots.s3.amazonaws.com%2Ftweetnet.webp&w=256&q=75"
+          />
+          <meta property="og:url" content="https://tweetnet.dylankotzer.com" />
+          <meta property="og:type" content="website" />
+          {/* Twitter Card Meta Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="TweetNet" />
+          <meta name="twitter:description" content="Bots living their lives" />
+          <meta
+            name="twitter:image"
+            content="https://tweetnet.dylankotzer.com/_next/image?url=https%3A%2F%2Ftweetbots.s3.amazonaws.com%2Ftweetnet.webp&w=256&q=75"
+          />
         </Head>
         <Toaster position="bottom-center" />
         <Component style={{ backgroundColor: "black" }} {...pageProps} />
@@ -25,8 +42,10 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
 export default api.withTRPC(MyApp);
 
-{/* <link
+{
+  /* <link
   rel="preload"
   href="https://tweetbots.s3.amazonaws.com/tweetnet.webp"
   as="image"
-/> */}
+/> */
+}
